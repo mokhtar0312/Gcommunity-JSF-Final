821bbdade56e46a29f5dfdad25b9deeb2055c94a
ghazi.belgaroui <ghazi.belgaroui@esprit.tn>
2016-01-11 18:05:16 +0100

    Ayoub Crud Tutorial+ vues

---
+++ gcommunity-jsf/gcommunity-jsf-web/src/main/java/tn/esprit/bean/TutorialBean.java
@@ -0,0 +1,135 @@
+package tn.esprit.bean;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import entities.SimpleMember;
+import entities.Tutorial;
+import services.interfaces.TutorialServicesRemote;
+
+@ManagedBean
+@RequestScoped
+public class TutorialBean {
+	
+	@EJB
+	TutorialServicesRemote servicesRemote;
+	
+	private List<Tutorial> tutorials;
+	private Tutorial m=new Tutorial();
+	private SimpleMember s;
+	
+	
+	private boolean showForm = false;
+	private Boolean dispTable = false;
+	private String name;
+	private int id;
+	private boolean edit;
+	private String searchh;
+	public List<Tutorial> getTutorials() {
+		return tutorials;
+	}
+	public void setTutorials(List<Tutorial> tutorials) {
+		this.tutorials = tutorials;
+	}
+	public Tutorial getM() {
+		return m;
+	}
+	public void setM(Tutorial m) {
+		this.m = m;
+	}
+	public boolean isShowForm() {
+		return showForm;
+	}
+	public void setShowForm(boolean showForm) {
+		this.showForm = showForm;
+	}
+	public Boolean getDispTable() {
+		return dispTable;
+	}
+	public void setDispTable(Boolean dispTable) {
+		this.dispTable = dispTable;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean isEdit() {
+		return edit;
+	}
+	public void setEdit(boolean edit) {
+		this.edit = edit;
+	}
+	public String getSearchh() {
+		return searchh;
+	}
+	public void setSearchh(String searchh) {
+		this.searchh = searchh;
+	}
+	public SimpleMember getS() {
+		return s;
+	}
+	
+	@PostConstruct
+	private void init() {
+	tutorials =servicesRemote.findAllTutorial();
+	}
+
+	public void doDisplayTable() {
+		dispTable = true;
+	}
+	
+	public String doCreateTutorial() {
+		
+		
+			
+			servicesRemote.addTutorial(m, s);
+		init();
+		m=new Tutorial();
+		dispTable = false;
+		
+		return "";
+	}
+public void edit(Tutorial item) {
+		
+		
+	    this.m = item;
+	    edit = true;
+	}
+	
+	public String doDeleteTutorial() {
+		servicesRemote.deleteTutorial(m);
+		init();
+		m=new Tutorial();
+		return "";
+	}
+	
+	public String doFindTutorialByTyped() {
+		tutorials=(List<Tutorial>) servicesRemote.FindbyTyped(searchh);
+		return "";
+	}
+	public String doUpdateTutorial(){
+		servicesRemote.updateTutorial(m);
+		init();
+		m=new Tutorial();
+		return "";
+	}
+	public String doFindTutorialdById(){
+		servicesRemote.findTutorialById(id);
+		return"";
+	}
+	
+	
+	
+
+}
